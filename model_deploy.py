002e11d8394e1a39d84ff5bdcda9894b407495f4
FlorianWoelki <FlorianWoelki@gmx.de>
2019-09-29 21:06:30 +0200

    fixed model predictions

--- model_deploy.py
+++ model_deploy.py
@@ -1,11 +1,14 @@
 from keras.applications.mobilenet import MobileNet
 from keras.layers import GlobalAveragePooling2D, Dense, Dropout, Flatten
 from keras.preprocessing import image
-from keras.models import Sequential
+from keras.models import Sequential, model_from_json
+from keras.applications.mobilenet import preprocess_input
 import numpy as np
+import json
 
-def setup_model_with_weights():
-  base_mobilenet_model = MobileNet(
+
+def build_model():
+  """base_mobilenet_model = MobileNet(
       input_shape=(128, 128, 3), 
       include_top=False,
       weights=None
@@ -16,24 +19,29 @@ def setup_model_with_weights():
   model.add(Dropout(0.5))
   model.add(Dense(512))
   model.add(Dropout(0.5))
-  model.add(Dense(14, activation='sigmoid'))
+  model.add(Dense(14, activation='sigmoid'))"""
+
+  with open('model_results/multi_disease_model.json', 'r') as json_file:
+    architecture = json.load(json_file)
+    model = model_from_json(json.dumps(architecture))
 
   model.load_weights('model_results/multi_disease_model_weight.h5')
 
-  model.compile(
+  """model.compile(
       optimizer='adam',
       loss='binary_crossentropy',
       metrics=['binary_accuracy', 'mae']
-  )
+  )"""
   return model
 
 
 def load_image(img_path):
   img = image.load_img(img_path, target_size=(128, 128))
-  img_tensor = image.img_to_array(img)
-  img_tensor = np.expand_dims(img_tensor, axis=0)
-  img_tensor /= 255.
-  return img_tensor
+  img = image.img_to_array(img)
+  img = np.expand_dims(img, axis=0)
+  img /= 255.
+  img = preprocess_input(img)
+  return img
 
 
 def predict_image(model, img_path, biggest_result=False):
@@ -45,6 +53,7 @@ def predict_image(model, img_path, biggest_result=False):
 labels = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Effusion',
           'Emphysema', 'Fibrosis', 'Hernia', 'Infiltration', 'Mass', 'Nodule',
           'Pleural_Thickening', 'Pneumonia', 'Pneumothorax']
-model = setup_model_with_weights()
-(index, value) = predict_image(model, 'model_results/__results___files/__results___10_0.png', biggest_result=True)
-print(labels[index], value)
+model = build_model()
+
+preds = predict_image(model, 'model_results/test_images/Atelectasis.png')
+print(preds)
